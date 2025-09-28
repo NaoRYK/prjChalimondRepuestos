de6b6277debb4ebc4039eecc260d37c52ad9e122
Nao <92193274+NaoRYK@users.noreply.github.com>
2025-09-27 23:16:13 -0300

    Agregar archivos de proyecto.

---
+++ prjChalimondRepuestos/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace prjChalimondRepuestos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            richTextBox1 = new RichTextBox();
+            numericUpDown2 = new NumericUpDown();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button2 = new Button();
+            comboBox3 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(30, 37);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(150, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(198, 37);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(150, 23);
+            comboBox2.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(30, 80);
+            numericUpDown1.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(30, 121);
+            richTextBox1.MaxLength = 50;
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(318, 96);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(198, 81);
+            numericUpDown2.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(150, 23);
+            numericUpDown2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Location = new Point(28, 18);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(363, 247);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingresar datos";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(28, 218);
+            button1.Name = "button1";
+            button1.Size = new Size(320, 23);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(comboBox3);
+            groupBox2.Location = new Point(397, 18);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(376, 247);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Consultar datos";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(175, 33);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(180, 199);
+            listBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(19, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 23);
+            textBox1.TabIndex = 9;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(19, 116);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(19, 91);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(19, 152);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 48);
+            button2.TabIndex = 6;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(19, 33);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(150, 23);
+            comboBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(198, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Origen";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(30, 62);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 15);
+            label3.TabIndex = 8;
+            label3.Text = "N° Repuesto";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(198, 63);
+            label4.Name = "label4";
+            label4.Size = new Size(40, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Precio";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(30, 106);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Descripcion";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 303);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private NumericUpDown numericUpDown1;
+        private RichTextBox richTextBox1;
+        private NumericUpDown numericUpDown2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private GroupBox groupBox2;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button2;
+        private ComboBox comboBox3;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label5;
+    }
+}
